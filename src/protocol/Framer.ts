e3775fad53602d2fb907b1e0e1add874724a2435
LIMPIX31 <limpix31@gmail.com>
2022-06-09 21:00:53 +0200

    fix framer

--- src/protocol/Framer.ts
+++ src/protocol/Framer.ts
@@ -1,5 +1,4 @@
 import varint from 'varint'
-import { Logger } from '../logger/Logger.js'
 
 export interface FrameData {
   data: Buffer,
@@ -10,43 +9,38 @@ export class Framer {
   private frames: FrameData[] = []
   private currentFrameIndex = 0
 
-  constructor(private readonly logger: Logger) {}
-
   push(data: Buffer): void {
-    try {
-      const nextFrame = this.frames[this.currentFrameIndex]
-      if (nextFrame) {
-        data.slice(nextFrame.length).copy(nextFrame.data, nextFrame.length)
-        const residue = data.length - nextFrame.length
-        if (residue >= 0) {
-          this.currentFrameIndex++
-          if (residue !== 0) this.push(data.slice(nextFrame.length))
-        }
-      } else {
-        const length = varint.decode(data)
-        const lastFrame = this.frames[this.frames.push({
-          length,
-          data: Buffer.alloc(length),
-        }) - 1]
-        data.slice(varint.decode.bytes, length + varint.decode.bytes).copy(lastFrame.data)
-        const residue = data.length - (varint.decode.bytes + length)
-        if (residue >= 0) {
-          this.currentFrameIndex++
-          if (residue !== 0) this.push(data.slice(length + varint.decode.bytes))
-        }
+    const nextFrame = this.frames[this.currentFrameIndex]
+    if (nextFrame) {
+      nextFrame.length += data.copy(nextFrame.data, nextFrame.length, 0, nextFrame.data.length)
+      const residue = data.length - nextFrame.data.length
+      if (residue >= 0) {
+        this.currentFrameIndex++
+        if (residue !== 0) this.push(data.slice(nextFrame.data.length))
       }
-    } catch (e: any) {
-      this.logger.debug(`Failed to frame chunk: ${e.message}`)
+    } else {
+      const length = varint.decode(data)
+      const lastFrame = this.frames[this.frames.push({
+        length: 0,
+        data: Buffer.alloc(length),
+      }) - 1]
+      lastFrame.length += data.copy(lastFrame.data, 0, varint.decode.bytes, length + varint.decode.bytes)
+      const residue = data.length - (varint.decode.bytes + length)
+      if (residue >= 0) {
+        this.currentFrameIndex++
+        if (residue !== 0) this.push(data.slice(length + varint.decode.bytes))
+      }
     }
   }
 
   ejectFrames(): Buffer[] {
     const frames = this.frames.filter(v => v.length === v.data.length).map(frame => frame.data)
+    this.frames = []
     this.currentFrameIndex = 0
     return frames as Buffer[]
   }
 
   hasFrames(): boolean {
-    return this.frames.length > 0
+    return this.frames.filter(v => v.length === v.data.length).length > 0
   }
 }
