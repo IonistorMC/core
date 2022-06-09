51c152dc430f73e94e0916bbf4242144b985edd7
LIMPIX31 <limpix31@gmail.com>
2022-06-09 20:59:07 +0200

    datatypes aliases

---
+++ src/dtalias.ts
@@ -0,0 +1,15 @@
+import { XYZPosition } from './protocol/datatypes/Position.js'
+
+export type byte = number
+export type short = number
+export type int = number
+export type long = bigint
+export type float = number
+export type double = number
+export type varint = number
+export type ubyte = number
+export type ushort = number
+export type uint = number
+export type ulong = bigint
+export type uuid = Uint8Array
+export type position = XYZPosition
