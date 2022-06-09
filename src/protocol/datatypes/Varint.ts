df988076ae67faa534d020ab8b95ac76f3bb8605
LIMPIX31 <limpix31@gmail.com>
2022-06-09 09:56:07 +0200

    varint update. Use lib

--- src/protocol/datatypes/Varint.ts
+++ src/protocol/datatypes/Varint.ts
@@ -1,36 +1,14 @@
 import { TypedBuffer } from '../TypedBuffer.js'
 import { DataType } from '../DataType.js'
+import varint from 'varint'
 
 export class Varint extends DataType<number> {
-  private readonly SEGMENT_BITS = 0x7f
-  private readonly CONTINUE_BIT = 0x80
-
   read(buffer: TypedBuffer, offset: number): [number, number] {
-    let result = 0
-    let localOffset = 0
-    let position = 0
-    let currentByte = 0
-    const exit = () => (currentByte & this.CONTINUE_BIT) === 0
-    do {
-      currentByte = buffer.buffer.readInt8(localOffset + offset)
-      localOffset++
-      result |= (currentByte & this.SEGMENT_BITS) << position
-      if (exit()) break
-      position += 7
-      if (position >= 32)
-        throw new Error('Varint is too big')
-    } while (!exit())
-    return [result, localOffset]
+    return [varint.decode(buffer.buffer, offset), varint.decode.bytes]
   }
 
   write(buffer: TypedBuffer, value: number): number {
-    let length = 0
-    while ((value & ~this.SEGMENT_BITS) != 0) {
-      buffer.buffer.writeUInt8((value & this.SEGMENT_BITS) | this.CONTINUE_BIT, buffer.length + length)
-      value >>>= 7
-      length++
-    }
-    buffer.buffer.writeUInt8(value, buffer.length + length)
-    return length + 1
+    varint.encode(value, buffer.buffer, buffer.length)
+    return varint.encode.bytes
   }
 }
