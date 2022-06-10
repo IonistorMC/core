0e0cf67c71fea98806a3e71fe8244bd28ac23a8b
LIMPIX31 <limpix31@gmail.com>
2022-06-10 16:22:47 +0200

    add .js ext

--- src/protocol/datatypes/Chat.ts
+++ src/protocol/datatypes/Chat.ts
@@ -1,6 +1,6 @@
 import { DataType } from '../DataType.js'
 import { JSONChatComponent } from '@ionistor/jcc'
-import { TypedBuffer } from '../TypedBuffer'
+import { TypedBuffer } from '../TypedBuffer.js'
 
 export class Chat extends DataType<JSONChatComponent> {
   read(buffer: TypedBuffer): [JSONChatComponent, number] {
